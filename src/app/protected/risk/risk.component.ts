264f599a55b489242db9f894f393272c57d19a51
Roberto Pablo Carreno Zambrano <r.carrenoz@uniandes.edu.co>
2022-11-05 22:17:33 -0500

    Refact risks

--- src/app/protected/risk/risk.component.ts
+++ src/app/protected/risk/risk.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Chart, registerables } from 'chart.js';
+import { Athlete, Risk } from './risk.interfaces';
+import { RiskService } from './risk.service';
 
 @Component({
   selector: 'app-risk',
@@ -7,20 +9,18 @@ import { Chart, registerables } from 'chart.js';
   styleUrls: ['./risk.component.css'],
 })
 export class RiskComponent implements OnInit {
-  user!: {
-    edad: number;
-    estatura: number;
-    imc: number;
-    deportes: Array<string>;
-  };
-  description!: {
-    desc: string;
-  };
-  constructor() {}
+  user: Athlete;
+  risk: Risk;
+  constructor(private riskService: RiskService,) {}
 
   ngOnInit(): void {
-    this.user = getUser();
-    this.description = getDescription();
+    this.riskService.getAthlete().subscribe(
+        athlete => {
+        this.user = athlete;
+        console.log(this.user);
+        this.risk = athlete.risks[0];  
+      }
+    );
     Chart.register(...registerables);
     var myChart = new Chart('chart', {
       type: 'bar',
@@ -57,18 +57,4 @@ export class RiskComponent implements OnInit {
       },
     });
   }
-}
-function getUser() {
-  return {
-    edad: 30,
-    estatura: 180,
-    imc: 20,
-    deportes: ['Atletismo', 'Ciclismo'],
-  };
-}
-
-function getDescription() {
-  return {
-    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum',
-  };
 }
